d2eb17c4e7df84a7ab4186d3778ce5b5aaa932c9
jakkarin <jakkarin@go.buu.ac.th>
2023-07-03 10:34:51 +0700

    add file LinearSearchExample

---
+++ src/LinearSearchExample.java
@@ -0,0 +1,24 @@
+public class LinearSearchExample {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return the index of the target element
+            }
+        }
+        return -1; // Return -1 if the target element is not found
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 9, 2, 7, 1, 6};
+        int target = 7;
+
+        int index = linearSearch(numbers, target);
+
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+}
+
